365abd18807b0237c41841a27e1a39c488310144
oklex <alexkim.347@gmail.com>
2020-06-03 19:20:36 -0700

    updated firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -4,25 +4,28 @@ import App from "./containers/App/App";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.scss";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import firebase from 'firebase'
+import firebase from "firebase";
+import dotenv from 'dotenv';
 
-require("dotenv").config(); // required for process.env
+require('dotenv').config()
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 try {
-    firebase.initializeApp({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-    })
+  let config: any = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  };
+  console.log('firebase config: ', config)
+  firebase.initializeApp(config);
 } catch (err) {
-    console.log("Firebase connection failed")
-    console.log(err)
+  console.log("Firebase connection failed");
+  console.log(err);
 }
 
 // If you want your app to work offline and load faster, you can change
