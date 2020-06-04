d30c9afd3491d30b848110dfd879ac2b7d1470a0
oklex <alexkim.347@gmail.com>
2020-06-04 11:34:50 -0700

    moved use key to component did mount

--- src/containers/Subscribe/Subscribe.tsx
+++ src/containers/Subscribe/Subscribe.tsx
@@ -3,52 +3,54 @@ import "./Subscribe.scss";
 import firebase from "firebase";
 
 class Subscribe extends React.Component<{}, {}> {
-  getNotifications = () => {
-    // Retrieve Firebase Messaging object.
-    const messaging = firebase.messaging();
-    const key: any = process.env.REACT_APP_PUBLIC_VAPID_KEY;
-    if (key !== undefined) {
-      messaging.usePublicVapidKey(key);
-      // Get Instance ID token. Initially this makes a network call, once retrieved
-      // subsequent calls to getToken will return from cache.
-      messaging
-        .getToken()
-        .then((currentToken) => {
-          if (currentToken) {
-            // sendTokenToServer(currentToken);
-            // updateUIForPushEnabled(currentToken);
-            console.log('currentToken: ', currentToken)
-          } else {
-            // Show permission request.
-            console.log(
-              "No Instance ID token available. Request permission to generate one."
-            );
-            // Show permission UI.
-            // updateUIForPushPermissionRequired();
-            // setTokenSentToServer(false);
-          }
-        })
-        .catch((err) => {
-          console.log("An error occurred while retrieving token. ", err);
-        //   showToken("Error retrieving Instance ID token. ", err);
-        //   setTokenSentToServer(false);
-        });
-    } else {
-        console.log('no web-notification key')
-      throw Error("no web-notification key");
-    }
-  };
+	componentDidMount = () => {
+		const messaging = firebase.messaging();
+		const key: any = process.env.REACT_APP_PUBLIC_VAPID_KEY;
+		if (key !== undefined) {
+			messaging.usePublicVapidKey(key);
+		} else {
+			console.log("no web-notification key");
+			throw Error("no web-notification key");
+		}
+	};
 
-  render() {
-    return (
-      <div className="subscribePage container">
-        {/* <h3>Subscribe to web-notifications</h3> */}
-        <button onClick={() => this.getNotifications()}>
-          set-up web notifications
-        </button>
-      </div>
-    );
-  }
+	getNotifications = () => {
+		// Retrieve Firebase Messaging object.
+		const messaging = firebase.messaging();
+		messaging
+			.getToken()
+			.then((currentToken) => {
+				if (currentToken) {
+					// sendTokenToServer(currentToken);
+					// updateUIForPushEnabled(currentToken);
+					console.log("currentToken: ", currentToken);
+				} else {
+					// Show permission request.
+					console.log(
+						"No Instance ID token available. Request permission to generate one."
+					);
+					// Show permission UI.
+					// updateUIForPushPermissionRequired();
+					// setTokenSentToServer(false);
+				}
+			})
+			.catch((err) => {
+				console.log("An error occurred while retrieving token. ", err);
+				//   showToken("Error retrieving Instance ID token. ", err);
+				//   setTokenSentToServer(false);
+			});
+	};
+
+	render() {
+		return (
+			<div className="subscribePage container">
+				{/* <h3>Subscribe to web-notifications</h3> */}
+				<button onClick={() => this.getNotifications()}>
+					set-up web notifications
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Subscribe;
