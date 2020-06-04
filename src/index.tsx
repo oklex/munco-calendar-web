763c86cccf68226de724cfc074e50cd3ef6e4a40
oklex <alexkim.347@gmail.com>
2020-06-03 19:04:05 -0700

    added firebase again

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,26 @@ import App from "./containers/App/App";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.scss";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import firebase from 'firebase'
 
 require("dotenv").config(); // required for process.env
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+try {
+    firebase.initializeApp({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+    })
+} catch (err) {
+    console.log("Firebase connection failed")
+    console.log(err)
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
